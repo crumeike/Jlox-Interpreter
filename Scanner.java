1beb2ee8e89f58b341fe6b95d259ead83f982d99
Chibuike Robinson <umeikerobinson@gmail.com>
2023-09-30 23:58:17 -0500

    Update exisiting files

--- Scanner.java
+++ Scanner.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.craftinginterpreters.lox.Lox;
+
 import static com.craftinginterpreters.lox.TokenType.*; 
 
 class Scanner {
@@ -41,10 +43,39 @@ class Scanner {
       case '+': addToken(PLUS); break;
       case ';': addToken(SEMICOLON); break;
       case '*': addToken(STAR); break; 
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+
+      default:
+        Lox.error(line, "Unexpected character.");
+        break;
     }
   }
+  
   private boolean isAtEnd() {
     return current >= source.length();
+  }
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
   }
 
 }
