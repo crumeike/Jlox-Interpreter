737e7f0edb65a4539a6d50af03e76323de45dfdf
Chibuike Robinson <umeikerobinson@gmail.com>
2023-10-04 23:48:08 -0500

    Updating existing file

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -12,5 +12,33 @@ public class GenerateAst {
       System.exit(64);
     }
     String outputDir = args[0];
+    defineAst(outputDir, "Expr", Arrays.asList(
+      "Binary   : Expr left, Token operator, Expr right",
+      "Grouping : Expr expression",
+      "Literal  : Object value",
+      "Unary    : Token operator, Expr right"
+    ));
+  }
+  private static void defineAst(
+      String outputDir, String baseName, List<String> types)
+      throws IOException {
+    String path = outputDir + "/" + baseName + ".java";
+    PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+    writer.println("package com.craftinginterpreters.lox;");
+    writer.println();
+    writer.println("import java.util.List;");
+    writer.println();
+    writer.println("abstract class " + baseName + " {");
+
+    // The AST classes.
+    for (String type : types) {
+      String className = type.split(":")[0].trim();
+      String fields = type.split(":")[1].trim(); 
+      defineType(writer, baseName, className, fields);
+    }
+
+    writer.println("}");
+    writer.close();
   }
 }
